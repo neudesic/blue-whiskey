a47056c075b25d5a0ad6faf75da39fa21264f1bb
Chris Holwerda <chris.holwerda@coxautoinc.com>
2020-06-12 13:55:56 -0500

    Enable Azure Monitoring for Containers & Prometheus

--- modules/cluster-config/aks-cluster.tf
+++ modules/cluster-config/aks-cluster.tf
@@ -18,6 +18,10 @@ resource "azurerm_kubernetes_cluster" "default" {
     kube_dashboard {
       enabled = true
     }
+    oms_agent{
+      enabled = true
+      log_analytics_workspace_id = azurerm_log_analytics_workspace.primary.id      
+    }
   }
 
   service_principal {
@@ -35,4 +39,33 @@ resource "azurerm_kubernetes_cluster" "default" {
 
   depends_on = [var.resource_group_name]
 
+}
+
+resource "random_id" "workspace" {
+  keepers = {
+    # Generate a new id each time we switch to a new resource group
+    group_name = var.resource_group_name
+  }
+
+  byte_length = 8
+}
+
+resource "azurerm_log_analytics_workspace" "primary" {
+  name                = "k8s-workspace-${random_id.workspace.hex}"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku                 = "PerGB2018"
+}
+
+resource "azurerm_log_analytics_solution" "primary" {
+  solution_name         = "ContainerInsights"
+  location              = var.location
+  resource_group_name   = var.resource_group_name
+  workspace_resource_id = azurerm_log_analytics_workspace.primary.id
+  workspace_name        = azurerm_log_analytics_workspace.primary.name
+
+  plan {
+    publisher = "Microsoft"
+    product   = "OMSGallery/ContainerInsights"
+  }
 }
